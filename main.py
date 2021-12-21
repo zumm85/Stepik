c86c7131f32cc0ed6ea2ece8a8a668ca2da36e50
zumm85 <77451156+zumm85@users.noreply.github.com>
2021-12-21 19:06:12 +0000

    2.06

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-m, n = int(input()), int(input())
-for i in range(m, n + 1):
-    if i % 17 == 0 or i % 10 == 9 or (i % 3 == 0 and i % 5 == 0):
-        print(i)
+n = int(input())
+for i in range(1, 11):
+    print(n, 'x', i, '=', (n * i))
