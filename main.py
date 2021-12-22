48cf8067f9ec4b9e2c91dd785fb22f1ff37792d7
zumm85 <77451156+zumm85@users.noreply.github.com>
2021-12-22 08:14:22 +0000

    15.14

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import math
 n = int(input())
-counter = 0
+sum = 0
 for i in range(1, n + 1):
-    num = int(input())
+    sum += 1 / (i)
 
-    counter += num
-print(counter)
+print((sum) - math.log(n))
