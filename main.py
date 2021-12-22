c2c7cf4b566db7dde7d00539362feff987db376a
zumm85 <77451156+zumm85@users.noreply.github.com>
2021-12-22 07:26:50 +0000

    14.26

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-a, b = int(input()), int(input())
+n = int(input())
 counter = 0
-for i in range(a, b + 1):
-    if i ** 3 % 10 == 4 or i ** 3 % 10 == 9:
-        counter += 1
+for i in range(1, n + 1):
+    num = int(input())
+
+    counter += num
 print(counter)
