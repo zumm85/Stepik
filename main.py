2138893e6c66b4d873fb68149ffccd71bcc5cacc
zumm85 <zumm85@yandex.ru>
2022-04-04 22:54:07 +0700

    22.54

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-n = int(input())
-fib = 1
-fib1 = 0
-for i in range(1, n + 1):
-    fib2 = fib
-    fib = fib2 + fib1
-    fib1 = fib2
-    print(fib1, end=' ')
+a = int(input())
+count = 0
+while a >= 25:
+    count += 1
+    a -= 25
+while a >= 10:
+    count += 1
+    a -= 10
+while a >= 5:
+    count += 1
+    a -= 5
+while a >= 1:
+    count += 1
+    a -= 1
+print(count)
