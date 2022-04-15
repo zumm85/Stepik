a81b7ce7c491ca0181ed7fd3c4b48b805c0aec72
zumm85 <zumm85@yandex.ru>
2022-04-15 21:12:16 +0700

    21.12

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 a = int(input())
-b = 0
-c = 0
-flag = True
-while a != 0:
-    b = a % 10
-    a = a // 10
-    c = a % 10
-    if c <= 0:
-        c = b
-#    print(b)
-#    print(c)
-    if b > c:
-        flag == False
-        print('NO')
-        break
-else:
-    print('YES')
+for i in range(1, a + 1):
+    for j in range(5):
+        print(i, end=' ')
+    print()
