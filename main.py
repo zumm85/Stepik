4a12c5c489a66ccaabe5eea38499af5a7b345252
zumm85 <zumm85@yandex.ru>
2022-04-18 16:48:36 +0700

    16.48

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = int(input())
-for i in range(1, n + 1):
-    for j in range(1, 10):
-        print(i, '+', j, '=', i + j)
-    print()
+a, b = int(input()), int(input())
+num_max = 0
+num_sum = 0
+for i in range(a, b + 1):
+    count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count += j
+            if count >= num_sum:
+                num_sum = count
+                num_max = j
+print(num_max, num_sum, sep=' ')
