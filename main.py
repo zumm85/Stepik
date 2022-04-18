aed722a3a0a0d57b9f5d792f4481858bb74f62d2
zumm85 <zumm85@yandex.ru>
2022-04-18 16:48:36 +0700

    16.48

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 n = int(input())
-for i in range(1, n + 1):
-    for j in range(1, 10):
-        print(i, '+', j, '=', i + j)
-    print()
+x3 = 0
+countlast = 0
+x2 = 0
+x5 = 0
+x7 = 1
+x05 = 0
+last = n % 10
+while n > 0:
+    a = n % 10
+    if a == 3:
+        x3 += 1
+    if a == last:
+        countlast += 1
+    if a % 2 == 0:
+        x2 += 1
+    if a >5:
+        x5 += a
+    if a > 7:
+        x7 *= a
+    if a == 0 or a == 5:
+        x05 += 1
+    n //= 10
+print(x3, countlast, x2, x5, x7, x05, sep="\n")
